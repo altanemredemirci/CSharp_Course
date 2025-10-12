63bfa2bf4125c8e8ed188a8611f63a3795fbc081
altanemredemirci <altan_emre_1989@hotmail.com>
2025-10-12 15:41:30 +0300

    while

--- 06_KararYapilari/Program.cs
+++ 06_KararYapilari/Program.cs
@@ -220,29 +220,179 @@ namespace _06_KararYapilari
             //int sayi = Convert.ToInt32(Console.ReadLine());
             //goto DON;
 
-            string sifre = "123";
-            int hak = 3;
+            //    string sifre = "123";
+            //    int hak = 3;
 
-        TEKRAR:
-            Console.WriteLine("Password:");
-            string pass = Console.ReadLine();
-            hak--;
+            //TEKRAR:
+            //    Console.WriteLine("Password:");
+            //    string pass = Console.ReadLine();
+            //    hak--;
 
-            if (pass == sifre)
-            {
-                Console.WriteLine("Tebrikler! Giriş Başarılı");
-            }
+            //    if (pass == sifre)
+            //    {
+            //        Console.WriteLine("Tebrikler! Giriş Başarılı");
+            //    }
 
-            else if(hak>0)
-            {
-                Console.WriteLine("Hata! Lütfen giriş bilgilerinizi kontrol ediniz");
-                goto TEKRAR;
-            }
+            //    else if(hak>0)
+            //    {
+            //        Console.WriteLine("Hata! Lütfen giriş bilgilerinizi kontrol ediniz");
+            //        goto TEKRAR;
+            //    }
 
             #endregion
 
             #region SWITCH CASE
             //Switch Case yapısı birden fazla koşulun olduğu durumlarda kullanılır.
+
+            #region Kullanıcıya haftanın kaçıncı günü olduğunu soralım ve girilen rakama göre gün adını ekrana yazdıralım.
+
+            //Console.WriteLine("Haftanın kaçıncı günündeyiz?");
+            //int gun = Convert.ToInt32(Console.ReadLine());
+
+            //switch (gun) // ==
+            //{
+            //    case 1:
+            //        Console.WriteLine("Pazartesi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Salı");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Çarşamba");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Perşembe");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Cuma");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Cumartesi");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Pazar");
+            //        break;
+
+            //    default: //else
+            //        Console.WriteLine("Hafta 7 günden oluşur.!!!");
+            //        break;
+            //}
+
+
+            #endregion
+            #region Klavyeden iki sayı, bir işlem(+,-,*,/) girilecek. Sayılar girilen işleme göre sonucu ekrana yazdıran switch case.
+
+            //Console.WriteLine("1.Sayı:");
+            //double s1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("2.Sayı:");
+            //double s2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //Console.WriteLine("İşlem(+,-,*,/):");
+            //string islem = Console.ReadLine();
+
+            //switch (islem)
+            //{
+            //    case "+":
+            //        Console.WriteLine(s1+s2);
+            //        break;
+            //    case "-":
+            //        Console.WriteLine(s1 - s2);
+            //        break;
+            //    case "*":
+            //        Console.WriteLine(s1 * s2);
+            //        break;
+            //    case "/":
+            //        Console.WriteLine(s1 / s2);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Hatalı İşlem!!");
+            //        break;
+            //}
+
+
+            #endregion
+            #region   
+            /*
+            kullanıcıdan alınan cinsiyet bilgisine göre
+            ==> ERKEK ise
+            yaşı 60 ve üstü ise maaşının 10 katı kadar ikramiye alaral emekli edilecek, yaş 60'ın altında ise çalıştığı gün sayısına göre eğer 6000 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek,    6000 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
+            ==> KADIN ise
+             yaşı 58 ve üstü ise maaşının 10 katı kadar ikramiye alarak emekli edilecek, yaş 58'ın altında ise çalıştığı gün sayısına göre eğer 3600 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek, 3600 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
+            ==> cinsiyet bilgisi switch-case ile sorgulanacak
+             */
+
+
+            Console.WriteLine("Cinsiyet:");
+            string cinsiyet = Console.ReadLine().ToUpper();
+
+            switch (cinsiyet)
+            {
+                case "ERKEK":
+                    Console.WriteLine("Yaşınız:");
+                    int yas = Convert.ToInt32(Console.ReadLine());
+
+                    if (yas >= 60)
+                    {
+                        Console.WriteLine("Maaş:");
+                        int maas = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("İkramiye:"+(maas*10));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prim Günü:");
+                        int prim = Convert.ToInt32(Console.ReadLine());
+
+                        if (prim >= 6000)
+                        {
+                            Console.WriteLine("Maaş:");
+                            int maas = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("İkramiye:" + (maas * 11));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Çalışmaya Devam..");
+                        }
+                    }
+                        break;
+
+                case "KADIN":
+                    Console.WriteLine("Yaşınız:");
+                    int yas2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (yas2 >= 58)
+                    {
+                        Console.WriteLine("Maaş:");
+                        int maas = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("İkramiye:" + (maas * 10));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prim Günü:");
+                        int prim = Convert.ToInt32(Console.ReadLine());
+
+                        if (prim >= 3600)
+                        {
+                            Console.WriteLine("Maaş:");
+                            int maas = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("İkramiye:" + (maas * 11));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Çalışmaya Devam..");
+                        }
+                    }
+                    break;
+            }
+
+
+
+            #endregion
             #endregion
 
         }
---
+++ 08_Döngüler/Program.cs
@@ -0,0 +1,157 @@
+﻿namespace _08_Döngüler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* LOOPS - DÖNGÜLER
+             * While
+             * For
+             * Do While
+             * Foreach
+           Döngüler belirli bir koşula bağlı olarak kendi içlerindeki kod bloğunu tekrarlı şekilde çalıştıran yazılım yapılarıdır.
+                       
+             
+             */
+
+            #region WHILE
+            /*
+            while (koşul) //koşul true olduğu sürece {} içerisindeki kodlar tekrar tekrar çalışır. Ne zaman koşul false olursa o zaman döngü biter.
+            {
+
+            }
+            */
+
+            //1-10 aralığında sayıları ekrana yazdırınız
+
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+
+            //int i = 1;
+
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+
+
+            //Kullanıcı 0 girene kadar sayı al ve alınan sayıları topla. 0 girdiğinde döngüyü bitir ve toplamı ekrana yaz
+
+            //Console.WriteLine("Sayı:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int toplam = 0;
+
+            //while (sayi != 0)
+            //{
+            //    toplam = toplam + sayi;
+
+            //    Console.WriteLine("Sayı:");
+            //    sayi = Convert.ToInt32(Console.ReadLine());
+            //}
+
+
+
+            //int toplam = 0;
+            //while (true)//Sonsuz döngü
+            //{
+            //    Console.WriteLine("Sayı:");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (sayi == 0)
+            //    {
+            //        Console.WriteLine("Toplam:"+toplam);
+            //        break;
+            //    }
+
+            //    toplam = toplam + sayi;
+            //}
+
+
+            #region 200 ile 0 arasındaki sayılardan 3'e tam bölünenleri ekrana yazdırınız.
+
+            //int sayi = 200;
+
+            //while (sayi > 0)
+            //{
+            //    if (sayi % 3 == 0)
+            //    {
+            //        Console.WriteLine(sayi);
+            //    }
+
+            //    sayi--;
+            //}
+
+
+            #endregion
+
+            #region 0-500 arasındaki sayılardan çiftleri ayrı tekleri ayrı toplayarak ekrana toplamları yazınız
+
+            //int sayi = 0;
+            //int tektoplam = 0;
+            //int cifttoplam = 0;
+
+            //while (sayi < 500)
+            //{
+            //    if (sayi % 2 == 0)
+            //    {
+            //        cifttoplam = cifttoplam + sayi;
+            //    }
+            //    else
+            //    {
+            //        tektoplam = tektoplam + sayi;
+            //    }
+            //    sayi++;                  
+            //}
+
+            //Console.WriteLine("Tek Toplam:"+tektoplam);
+            //Console.WriteLine("Çift Toplam:"+cifttoplam);
+
+            #endregion
+
+            #region BREAK VE CONTINUE KOMUTLARI
+
+            //Break:Okunduğu satırda döngü işlemini bitirir.
+
+            //Continue:Okunduğu satırdan döngü satırına geri döner.
+
+            //1-20 arasındaki sayıları toplayınız
+            //7 sayısı toplama işleminde atlansın
+            //sayı 15 olduğunda döngü bitsin
+
+            int sayi = 1;
+
+            int toplam = 0;
+            while (sayi < 20)
+            {
+                if (sayi==7)
+                {
+                    //7 sayısı toplama dahil edilmeyecek
+                    sayi++;
+                    continue;
+                }
+                if (sayi == 15)
+                {
+                    break;
+                }
+                toplam += sayi; //toplam=toplam+sayi
+                sayi++;
+
+            }
+            Console.WriteLine(toplam);
+
+
+            #endregion
+
+
+            #endregion
+
+        }
+    }
+}
